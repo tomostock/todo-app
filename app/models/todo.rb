e2d87162891e59a0f84e35ebc6fdb3c661b23704
tomostock <tomos.tomomi@gmail.com>
2021-11-15 12:34:09 +0900

    Todo data edit and add validates

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,5 @@
 class Todo < ApplicationRecord
+  validates :title, presence: true
+  validates :content, presence: true
+  validates :del_flg, inclusion: { in: [true, false] }
 end
--- db/migrate/20211115024805_create_todos.rb
+++ db/migrate/20211115024805_create_todos.rb
@@ -2,8 +2,8 @@ class CreateTodos < ActiveRecord::Migration[6.1]
   def change
     create_table :todos do |t|
       t.string :title
-      t.string :content
-      t.integer :del_flg
+      t.text :content
+      t.boolean :del_flg, default: false, null: false
 
       t.timestamps
     end
