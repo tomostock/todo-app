ca6ca758a969f513e85208dd76347270ff4451d7
tomostock <tomos.tomomi@gmail.com>
2021-11-15 16:38:57 +0900

    todo model test FactoryBot導入

---
+++ spec/factories/todo.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :todo do
+    title { 'title1' }
+    content { 'content1' }
+    status { 1 }
+  end
+end
--- spec/model/todo_spec.rb
+++ spec/model/todo_spec.rb
@@ -3,21 +3,13 @@ require 'rails_helper'
 RSpec.describe Todo, type: :model do
 
   it "タイトル、コンテンツ、フラグがある場合、有効である" do
-    todo = Todo.new(
-      title: "title1",
-      content: "content1",
-      status:  1,
-      )
+    todo = FactoryBot.build(:todo)
     # オブジェクトをexpectに渡した時に、有効である(be valid)という意味になります
     expect(todo).to be_valid
   end
 
   it "タイトルがない場合、無効である" do
-    todo = Todo.new(
-      title: nil,
-      content: "content1",
-      status:  1,
-      )
+    todo = FactoryBot.build(:todo, title: nil)
     todo.valid?
     # valid?メソッドでfalseであれば、user.errosでどんなerrorを持っているか返してくれます。今回は特に[:first_name]のエラーがみたいのでexpect()内部で指定してあげます。
     # 今回は"can't be blank"というエラーを含んでいる(include)しているはずという記述になります。複数含む場合ももちろんあります。
@@ -25,21 +17,13 @@ RSpec.describe Todo, type: :model do
   end
 
   it "コンテンツがない場合、無効である" do
-    todo = Todo.new(
-      title: "title1",
-      content: nil,
-      status:  1,
-      )
+    todo = FactoryBot.build(:todo, content: nil)
     todo.valid?
     expect(todo.errors[:content]).to include("can't be blank")
   end
 
   it "デリートフラグがない場合、無効である" do
-    todo = Todo.new(
-      title: "title1",
-      content: "content1",
-      status:  nil,
-      )
+    todo = FactoryBot.build(:todo, status: nil)
     todo.valid?
     expect(todo.errors[:status]).to include("can't be blank")
   end
