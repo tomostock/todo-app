90af588c06379a3172dbae50030d500c93a05f36
tomostock <tomos.tomomi@gmail.com>
2021-11-15 11:49:28 +0900

    generate Todo model

---
+++ db/migrate/20211115024805_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.string :content
+      t.integer :del_flg
+
+      t.timestamps
+    end
+  end
+end
